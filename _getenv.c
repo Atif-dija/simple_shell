f3ca7ee43139c87d930be5331a2de63fa5393884
Atif-dija <khadijaaif2@gmail.com>
2023-09-18 11:59:00 -0700

    tasks

---
+++ _getenv.c
@@ -0,0 +1,32 @@
+#include "shell.h"
+
+/**
+ * _getenv - retrieve the value of an environment variable
+ *
+ * @var: a pointer to a null-terminated string containing the name of the environment variable
+ *
+ * Return: a pointer to the value associated with that variable if it exists
+ * or NULL
+ */
+
+char *_getenv(char *var)
+{
+	char *tmp, *val, *env, *key;
+	int i;
+
+	for (i = 0; environ[i]; i++)
+	{
+		tmp = _strdup(environ[i]);
+		key = strtok(tmp, "=");
+		if (_strcmp(key, var) == 0)
+		{
+			val = strtok(NULL, "\n");
+			env = _strdup(val);
+			free(tmp);
+			return (env);
+		}
+		free(tmp);
+		tmp = NULL;
+	}
+	return (NULL);
+}
--- string.c
+++ string.c
@@ -36,15 +36,19 @@ char *_strcpy(char *dest, const char *src)
 
 int _strcmp(char *s1, char *s2)
 {
-		int n = 0;
+	int cmp;
 
-		while (s1[n] != '\0' && s2[n] != '\0')
-		{
-			if (s1[n] != s2[n])
-				return (s1[n] - s2[n]);
-			n++;
-		}
-		return (0);
+	cmp = (int)*s1 - (int)*s2;
+
+	while (*s1)
+	{
+		if (*s1 != *s2)
+			break;
+		s1++;
+		s2++;
+		cmp = (int)*s1 - (int)*s2;
+	}
+	return (cmp);
 }
 
 /**
